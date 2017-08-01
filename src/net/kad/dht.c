912cd8cdcbc02f7ed7a6577719d4d0d3b698c2c5
foudil <username@users.noreply.github.com>
2017-08-02 00:04:51 +0200

    Add find-closest-nodes sketch.

--- src/net/kad/dht.c
+++ src/net/kad/dht.c
@@ -85,6 +85,77 @@ static inline size_t kad_bucket_hash(const kad_guid *self_id,
     return bucket_idx;
 }
 
+/**
+ * Returns k nodes closest to the target node. Aka lookup.
+ *
+ * Traverse the routing table in ascending xor distance order relative to the
+ * target key. http://stackoverflow.com/a/30655403/421846
+ */
+/*
+   Ex: 4 bits space, for node 1010,
+   bucket 0, node  1011
+   bucket 1  nodes 100x (ex: 1001)
+   bucket 2  nodes 11xx
+   bucket 3  nodes 0xxx
+
+   Ex: 4 bits space, for node 1001,
+   bucket 0  node  1000
+   bucket 1  nodes 101x
+   bucket 2  nodes 11xx
+   bucket 3  nodes 0xxx
+
+   Ex: 4 bits space, for node 1100,
+   bucket 0  node  1101
+   bucket 1  nodes 111x
+   bucket 2  nodes 10xx
+   bucket 3  nodes 0xxx
+
+   Ex: 4 bits space, for node 0011,
+   bucket 0  node  0010
+   bucket 1  nodes 000x
+   bucket 2  nodes 01xx
+   bucket 3  nodes 1xxx
+
+   0. suppose node 1010, find_closest for 1001,
+   while not (k nodes added, or all buckets visited) do
+   1. find the bucket for target (1001 -> 1) [add bucket nodes], note prefix (= space_len - bucket)
+   2. next bucket is: target XOR shifted prefix:
+   (1001 XOR 0010 = 1011 => bucket 0)
+   (1001 XOR 0100 = 1101 => bucket 2)
+   (1001 XOR 1000 = 0001 => bucket 3)
+   all buckets visited
+
+   0. suppose node 1010, find_closest for 1100
+   while not (k nodes added, or all buckets visited):
+   1. find the bucket for target (1100 -> 2), this will be the initial prefix_len (2)
+   while prefix_len > 0:
+     next bucket is: target XOR shifted prefix:
+     (1100 XOR 0100 => 1000 => bucket 1)
+     (1100 XOR 1000 => 0100 => bucket 3)
+     (prefix_len/bucket_max_reached)
+   continue with unvisited => initial prefix downwards,
+   while prefix_len < space_len, just traverse sequentially:
+     (bucket 0)
+
+   0. suppose node 1010, find_closest for 0011
+   while not (k nodes added, or all buckets visited):
+   1. find the bucket for target (0011 -> 3), this will be the initial prefix_len (3)
+   while prefix_len > 0:
+     (prefix_len/bucket_max_reached)
+   continue with unvisited => initial prefix downwards,
+   while prefix_len < space_len, just traverse sequentially:
+     (bucket 2)
+     (bucket 1)
+     (bucket 0)
+*/
+bool dht_find_closest(struct kad_dht *dht, const kad_guid *target)
+{
+    // TODO:
+    (void)dht;
+    (void)target;
+    return true;
+}
+
 static inline size_t kad_bucket_count(const struct list_item *bucket)
 {
     size_t count = 0;
