b16196fe9c86d870e57afdb64319effc184e53eb
foudil <username@users.noreply.github.com>
2020-04-14 02:57:02 +0200

    Add heap/priority-queue data structure

---
+++ src/utils/heap.h
@@ -0,0 +1,108 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+/**
+ * A binary heap / priority queue implementation using a dynamic array.
+ *
+ * User MUST provide a comparison function:
+ *
+ *   int name##_cmp(const HEAP_ITEM_TYPE *a, const HEAP_ITEM_TYPE *b);
+ *
+ * Implemented as a max-heap, so user can just reverse the _cmp function to
+ * otain a min-heap.
+ */
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HEAP_SIZE_INITIAL 64
+#define HEAP_SIZE_FACTOR 2
+
+#define LEFT(i)   (2*(i) + 1)
+#define RIGHT(i)  (2*(i) + 2)
+#define PARENT(i) (size_t)(((i) - 1)/2)
+
+#define HEAP_GENERATE(name, type)     \
+    HEAP_GENERATE_BASE(name, type)    \
+    HEAP_GENERATE_INSERT(name, type)  \
+    HEAP_GENERATE_GET(name, type)
+
+#define HEAP_INIT(var_name, heap_name, item_type, capa)     \
+    struct heap_name var_name = {0};                        \
+    var_name.items = calloc(capa, sizeof(item_type));       \
+    var_name.cap = capa
+
+#define HEAP_GENERATE_BASE(name, type)                                  \
+    struct name {                                                       \
+        size_t  len;                                                    \
+        size_t  cap;                                                    \
+        type   *items;                                                  \
+    };                                                                  \
+                                                                        \
+static inline                                                           \
+bool name##_grow(struct name *h) {                                      \
+    /* FIXME overflow */                                                \
+    size_t cap_new = h->cap * 2;                                        \
+    type *items_new = realloc(h->items, sizeof(*h->items) * cap_new);    \
+    if (items_new == NULL)                                              \
+        return false;                                                   \
+    h->cap = cap_new;                                                   \
+    h->items = items_new;                                               \
+    return true;                                                        \
+}                                                                       \
+                                                                        \
+static inline void name##swap(type *a, type *b) {                       \
+    type tmp = *b;                                                      \
+    *b = *a;                                                            \
+    *a = tmp;                                                           \
+}
+
+
+#define HEAP_GENERATE_INSERT(name, type)        \
+/**
+ * Can fail if growing fails.
+ */                                                                     \
+bool name##_insert(struct name *h, type item)                           \
+{                                                                       \
+    if (h->len >= h->cap && !name##_grow(h))                            \
+        return false;                                                   \
+    h->items[h->len] = item;                                            \
+    h->len++;                                                           \
+                                                                        \
+    size_t i = h->len - 1;                                              \
+    while (i > 0 && name##_cmp(h->items[PARENT(i)], h->items[i]) < 0) { \
+        name##swap(&h->items[PARENT(i)], &h->items[i]);                 \
+        i = PARENT(i);                                                  \
+    }                                                                   \
+                                                                        \
+    return true;                                                        \
+}
+
+#define HEAP_GENERATE_GET(name, type)                                   \
+type name##_get(struct name *h)                                         \
+{                                                                       \
+    if (h->len == 0)                                                    \
+        /* FIXME ok for pointers, but not for other types */            \
+        return (type)0;                                                 \
+                                                                        \
+    type ret = h->items[0];                                             \
+    h->len--;                                                           \
+    h->items[0] = h->items[h->len];                                     \
+    h->items[h->len] = (type)0;                                         \
+                                                                        \
+    size_t i = 0;                                                       \
+    while ((i < h->len) &&                                              \
+           ((LEFT(i) < h->len && (name##_cmp(h->items[i], h->items[LEFT(i)]) < 0)) || \
+            (RIGHT(i) < h->len && name##_cmp(h->items[i], h->items[RIGHT(i)]) < 0))) { \
+        size_t child =                                                  \
+            (name##_cmp(h->items[LEFT(i)], h->items[RIGHT(i)]) < 0) ? \
+            RIGHT(i) : LEFT(i);                                         \
+        name##swap(&h->items[i], &h->items[child]);                     \
+        i = child;                                                      \
+    }                                                                   \
+                                                                        \
+    return ret;                                                         \
+}
+
+
+#endif /* HEAP_H */
---
+++ tests/utils/heap.c
@@ -0,0 +1,85 @@
+/* Copyright (c) 2017 Foudil Brétel.  All rights reserved. */
+#include <assert.h>
+#include <string.h>
+#include "utils/safer.h"
+#include "utils/heap.h"
+
+static inline int int_heap_cmp(const int a, const int b)
+{
+    return a - b;
+}
+HEAP_GENERATE(int_heap, int)
+
+
+struct some {
+    char c;
+};
+
+static inline int min_heap_cmp(const struct some *a, struct some *b)
+{
+    return b->c - a->c;
+}
+HEAP_GENERATE(min_heap, struct some *)
+
+#define N 10
+
+
+int main ()
+{
+    HEAP_INIT(ints, int_heap, int, 4);
+    assert(memcmp(ints.items, (int[N]){0}, N) == 0);
+
+    for (size_t i=0; i<N; ++i)
+        int_heap_insert(&ints, i);
+
+    /*
+      |         9
+      |     8       5
+      |   6   7   1   4
+      |  0 3 2
+     */
+    int expect_ints[] = {9,8,5,6,7,1,4,0,3,2};
+    assert(ints.len == N);
+    assert(memcmp(ints.items, expect_ints, N) == 0);
+
+    for (size_t i=0; i<N; ++i)
+        assert(int_heap_get(&ints) == N - 1 - (int)i);
+    assert(memcmp(ints.items, (int[N]){0}, N) == 0);
+
+    free_safer(ints.items);
+
+
+    HEAP_INIT(somes, min_heap, struct some *, 4);
+    assert(memcmp(somes.items, (int[N]){0}, N) == 0);
+
+    struct some have[N] = {{74},{73},{72},{71},{70},{69},{68},{67},{66},{65}};
+
+    for (size_t i=0; i<N; ++i)
+        min_heap_insert(&somes, &have[i]);
+    assert(somes.len == N);
+
+    /*
+      |            65
+      |      66         69
+      |   68    67    73  70
+      | 74 71  72
+     */
+    struct some expect_some[N] = {{65},{66},{69},{68},{67},{73},{70},{74},{71},{72}};
+    for (size_t i=0; i<N; ++i) {
+        /* printf("%d\n",somes.items[i]->c); */
+        assert(somes.items[i]->c == expect_some[i].c);
+    }
+
+    for (size_t i=0; i<N; ++i){
+        struct some *got = min_heap_get(&somes);
+        assert(got->c == have[N - 1 - (int)i].c);
+    }
+    assert(memcmp(somes.items, (int[N]){0}, N) == 0);
+
+    free_safer(somes.items);
+
+
+
+
+    return 0;
+}
